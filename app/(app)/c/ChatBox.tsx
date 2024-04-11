368a09d0fe1a7e986866143dd07486442e9893da
Darshan Gowda <96766940+darshan-sr@users.noreply.github.com>
2024-04-11 10:55:27 +0000

    form input UI

--- app/(app)/c/ChatBox.tsx
+++ app/(app)/c/ChatBox.tsx
@@ -13,6 +13,16 @@ import { spinner } from "@/components/chat/spinner";
 import { getChats, updateChats } from "@/actions/actions";
 import { Message } from "ai";
 import useChatStore from "@/store/chat-store";
+import Textarea from "react-textarea-autosize";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { IconArrowElbow, IconPlus } from "@/components/ui/icons";
+import ReactTextareaAutosize from "react-textarea-autosize";
 
 type ChatBoxProps = {
   diagramId: string;
@@ -26,6 +36,7 @@ export default function ChatBox({ diagramId, code, onChange }: ChatBoxProps) {
   const [hasResponseStarted, setHasResponseStarted] = useState(false);
   const { chat, fetchChat } = useChatStore();
   const [isMounted, setIsMounted] = useState(false);
+  const inputRef = React.useRef<HTMLTextAreaElement>(null);
   useEffect(() => {
     const fetchData = async () => {
       setInitialChats(chat);
@@ -102,24 +113,69 @@ export default function ChatBox({ diagramId, code, onChange }: ChatBoxProps) {
           {isLoading && !hasResponseStarted && <SpinnerMessage />}
         </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="absolute  flex justify-center border-t border-neutral-200 dark:border-neutral-800 items-center bottom-8 w-full   ">
-            <CirclePlus
+        <form
+          onSubmit={handleSubmit}
+          className="w-full relative flex items-center justify-center"
+        >
+          <div className="relative flex max-h-60 mx-4  w-11/12 grow flex-col overflow-hidden  px-8 sm:rounded-lg sm:border sm:px-12 bg-neutral-950">
+            {/* <CirclePlus
               size={25}
               className="absolute left-[4%] dark:text-neutral-200 rounded-full ml-4 z-50 p-1"
+            /> */}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="absolute left-0 top-[50%] translate-y-[-50%] size-8 rounded-full  p-0 sm:left-4"
+                  >
+                    <IconPlus />
+                    <span className="sr-only">New Chat</span>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>New Chat</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            <ReactTextareaAutosize
+              tabIndex={0}
+              placeholder="Send a message."
+              className="min-h-[30px] w-11/12 resize-none bg-transparent px-2 py-4 focus-within:outline-none sm:text-sm"
+              autoFocus
+              spellCheck={false}
+              autoComplete="off"
+              autoCorrect="off"
+              name="message"
+              rows={1}
+              value={input}
+              onChange={handleInputChange}
             />
-            <input
+
+            {/* <input
               value={input}
               className="h-10 mx-4  w-11/12 py-4 px-12 rounded-lg focus:ring-0 bg-neutral-200/50 dark:bg-neutral-700/50 backdrop-blur-md dark:bg-transparent/6 "
               placeholder="Ask AI "
               onChange={handleInputChange}
-            ></input>
-            <button
+            ></input> */}
+            <div className="absolute right-0 top-[50%] translate-y-[-50%]  sm:right-4 ">
+              <Button type="submit" size="icon" disabled={input === ""}>
+                <IconArrowElbow />
+              </Button>
+
+              {/* <button
+                type="submit"
+                className="absolute right-0 top-[50%] translate-y-[-50%] text-white rounded-full   bg-blue-600 "
+              >
+                <ArrowUp size={24} className="p-1" />
+              </button> */}
+            </div>
+            {/* <button
               type="submit"
-              className="absolute right-[4%] text-white rounded-full mr-4 bg-blue-600 "
+              className="absolute right-[4%] text-white rounded-full  mr-4 bg-blue-600 "
             >
               <ArrowUp size={24} className="p-1" />
-            </button>
+            </button> */}
           </div>
         </form>
       </div>
