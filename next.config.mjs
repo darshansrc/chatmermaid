6731c239b00c4c6e216b71c4f5149d1090ccb6fd
darshan-sr <darshansr1618@gmail.com>
2024-05-12 18:30:10 +0530

    bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// import { withContentlayer } from "next-contentlayer";
-// const nextConfig = withContentlayer({
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "lh3.googleusercontent.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "randomuser.me",
-//       },
-//     ],
-//   },
-//   experimental: {
-//     // serverComponentsExternalPackages: ["@prisma/client"],
-//     turbo: {
-//       rules: {
-//         "*.svg": {
-//           loaders: ["@svgr/webpack"],
-//           as: "*.js",
-//         },
-//       },
-//     },
-//   },
-// });
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+import { withContentlayer } from "next-contentlayer";
+const nextConfig = withContentlayer({
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -54,6 +19,41 @@ const nextConfig = {
       },
     ],
   },
-};
+  experimental: {
+    // serverComponentsExternalPackages: ["@prisma/client"],
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+});
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatars.githubusercontent.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "randomuser.me",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
